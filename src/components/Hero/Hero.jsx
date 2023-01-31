2b839f99aa71276e75d2674d2f72be099c7b29ab
Gabriel <gabrielloures21@gmail.com>
2023-01-31 14:00:08 -0300

    End Left Side Hero Component

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -19,7 +19,51 @@ const Hero = () => {
         </div>
 
         {/* Hero Heading */}
+        <div className="hero-text">
 
+          <div>
+            <span>Entre em </span>
+            <span className="stroke-text">Forma</span>
+          </div>
+
+          <div>
+            <span>Com a melhor</span>
+          </div>
+
+          <div className="p-text">
+            <span>Na nossa academia, vamos ajudá-lo a entrar em forma de maneira rápida e prática com os melhores treinos da atualidade</span>
+          </div>
+
+        </div>
+
+        {/* Figures */}
+        <div className="figures">
+
+          <div>
+            <span>+140</span>
+            <span>Personal Trainers</span>
+          </div>
+
+          <div>
+            <span>+978</span>
+            <span>Alunos matriculados</span>
+          </div>
+
+          <div>
+            <span>+50</span>
+            <span>Atividades físicas</span>
+          </div>
+
+        </div>
+
+        {/* Hero Buttons */}
+        <div className="hero-buttons">
+
+          <button className="btn">Comece Agora</button>
+          <button className="btn">Saiba Mais</button>
+
+        </div>
+        
       </div>
 
       <div className="right-h">
