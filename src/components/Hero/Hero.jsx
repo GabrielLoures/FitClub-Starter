2ef0256b72e0f29efac5bb9166e98021e7616d05
Gabriel <gabrielloures21@gmail.com>
2023-01-31 22:27:46 -0300

    End Hero Component

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import Header from '../Header/Header'
 
+import hero_image from "../../assets/hero_image.png"
+import hero_image_back from "../../assets/hero_image_back.png"
+import heart from "../../assets/heart.png"
+import calories from "../../assets/calories.png"
+
 import "./Hero.css"
 
 const Hero = () => {
@@ -67,7 +72,29 @@ const Hero = () => {
       </div>
 
       <div className="right-h">
-        RIGHT SIDE
+        
+        <button className="btn">Matricule-se</button>
+
+        <div className="heart-rate">
+          <img src={heart} alt="Imagem de um coração laranja" />
+          <span>Batimentos</span>
+          <span>120 bpm</span>
+        </div>
+
+        {/* Hero Images */}
+        <img src={hero_image} alt="Homem amarrando o tênis" className="hero-image" />
+        <img src={hero_image_back} alt="Imagem decorativa" className="hero-image-back" />
+
+        {/* Calories */}
+
+        <div className="calories">
+          <img src={calories} alt="Imagem de barras laranjas" />
+          <div>
+            <span>Calorias</span>
+            <span>220kcal</span>
+          </div>
+        </div>
+
       </div>
 
     </div>
