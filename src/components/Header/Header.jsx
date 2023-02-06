5aa4a41dcf1be2efb1121ee50f94f6de5171ca73
Gabriel <gabrielloures21@gmail.com>
2023-02-06 15:12:05 -0300

    End Responsive Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,22 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import "./Header.css"
 
 import Logo from "../../assets/logo.png"
+import Bars from "../../assets/bars.png"
 
 const Header = () => {
+
+  const mobile = window.innerWidth <= 768 ? true : false
+  const [menuOpened, setMenuOpened] = useState(false)
+
   return (
     <div className="header">
 
       <img className="logo" src={Logo} alt="Logo da empresa" />
 
-      <ul className="header-menu">
-        <li>Home</li>
-        <li>Programas</li>
-        <li>Sobre Nós</li>
-        <li>Planos</li>
-        <li>Depoimentos</li>
-      </ul>
+      {(menuOpened === false && mobile === true) ? (
+        <div 
+          className='sand-bars'
+          onClick={() => setMenuOpened(true)}  
+        >
+          <img src={Bars} alt="Menu sanduíche" />
+        </div>
+      ) : (
+        <ul className="header-menu">
+          <li onClick={() => setMenuOpened(false)}>Home</li>
+          <li onClick={() => setMenuOpened(false)}>Programas</li>
+          <li onClick={() => setMenuOpened(false)}>Sobre Nós</li>
+          <li onClick={() => setMenuOpened(false)}>Planos</li>
+          <li onClick={() => setMenuOpened(false)}>Depoimentos</li>
+        </ul>
+      )}
 
     </div>
   )
