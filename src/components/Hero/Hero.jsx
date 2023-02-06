6df242af5c7a30a13459da4cd9d9c5a070575135
Gabriel <gabrielloures21@gmail.com>
2023-02-06 15:28:00 -0300

    End Counting Number Hero Component (Rev 2)

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -8,6 +8,7 @@ import heart from "../../assets/heart.png"
 import calories from "../../assets/calories.png"
 
 import { motion } from "framer-motion"
+import NumberCounter from "number-counter"
 
 const Hero = () => {
   
@@ -57,17 +58,38 @@ const Hero = () => {
         <div className="figures">
 
           <div>
-            <span>+140</span>
+            <span>
+              <NumberCounter 
+                end={140} 
+                start={100} 
+                delay='4'
+                preFix='+'
+              />
+            </span>
             <span>Personal Trainers</span>
           </div>
 
           <div>
-            <span>+978</span>
+            <span>
+              <NumberCounter 
+                end={978} 
+                start={800} 
+                delay='4'
+                preFix='+'
+              />
+            </span>
             <span>Alunos matriculados</span>
           </div>
 
           <div>
-            <span>+50</span>
+            <span>
+              <NumberCounter 
+                end={50} 
+                start={0} 
+                delay='2'
+                preFix='+'
+              />
+            </span>
             <span>Atividades físicas</span>
           </div>
 
